438eede98af9199b90745882788219090dc55c8a
test <test@9750-5007-6974>
2024-04-04 11:55:21 +0200

    add static tags content

--- Block/CustomTab.php
+++ Block/CustomTab.php
@@ -8,6 +8,37 @@ class CustomTab extends Template
 {
     public function getCustomData()
     {
-        return "This is dynamic data from CustomTab.php";
+        $data = [
+            'eng_tags' => [
+                'Color' => 'Black And White',
+                'Department' => 'Sets',
+                'Detail' => 'Pocket',
+                'Fit' => 'Oversized',
+                'Neckline' => 'Round Neck',
+                'Pattern' => 'Plain',
+                'Sleeve-Length' => 'Long Sleeve',
+                'Style' => 'Casual',
+                'Type' => 'Sweatshirt',
+                'Sleeve Type' => 'Drop Shoulder',
+                'Target-Audience' => 'Women',
+                'Title' => 'drop shoulder pullover & wide leg pants'
+            ],
+            'ar_tags' => [
+                'اللون' => 'أسود وأبيض',
+                'القسم' => 'الأطقم',
+                'التفصيل' => 'جيب',
+                'المقاس' => 'كبير الحجم',
+                'قصة العنق' => 'العنق المستدير',
+                'نمط' => 'سادة',
+                'طول الأكمام' => 'أكمام طويلة',
+                'ستايل' => 'كاجوال',
+                'النوع' => 'سويتشيرت',
+                'نوع الأكمام' => 'الكتف المنخفض',
+                'الجمهور المستهدف' => 'نساء',
+                'Title' => '"سترة بأكمام واسعة وبنطلون واسع"'
+            ]
+        ];
+
+        return $data;
     }
 }
