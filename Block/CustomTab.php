2cc7293462e624bddf98b72ebd4c8cb27de5cd60
test <test@9750-5007-6974>
2024-04-16 10:47:29 +0200

    add logs

--- Block/CustomTab.php
+++ Block/CustomTab.php
@@ -3,10 +3,22 @@
 namespace TwentyToo\AutoTag\Block;
 
 use Magento\Framework\View\Element\Template;
+use Psr\Log\LoggerInterface;
 
 class CustomTab extends Template
 {
     protected $_tableName = 'twentytoo_tags';
+    protected $logger;
+
+    public function __construct(
+        Template\Context $context,
+        LoggerInterface $logger,
+        array $data = []
+    ) {
+        $this->logger = $logger;
+        parent::__construct($context, $data);
+    }
+
     public function getCustomData()
     {
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
@@ -20,6 +32,9 @@ class CustomTab extends Template
         $binds = [':order_id' => $staticOrderId];
 
         $results = $connection->fetchAll($select, $binds);
+
+        // Log the results array
+        $this->logger->info('Results array:', $results);
 
         return $results;
     }
