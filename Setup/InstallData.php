39758bbe7282c4acee2894c98c2c9982bc2d6604
test <test@9750-5007-6974>
2024-04-23 12:16:01 +0200

    update

---
+++ Setup/InstallData.php
@@ -0,0 +1,51 @@
+<?php
+// File: app/code/Vendor/Module/Setup/InstallData.php
+
+namespace Vendor\Module\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Psr\Log\LoggerInterface;
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * InstallData constructor.
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Install data method
+     *
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        // Your logic to trigger the logging goes here
+        $this->triggerLog();
+    }
+
+    /**
+     * Method to log your message
+     */
+    private function triggerLog()
+    {
+        // Your message to be logged
+        $message = 'Triggered on module installation';
+
+        // Log the message
+        $this->logger->info($message);
+    }
+}
