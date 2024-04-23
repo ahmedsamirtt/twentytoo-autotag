8d88f8c00142de885ef6f85eaa9659944a7ab02a
test <test@9750-5007-6974>
2024-04-23 16:51:27 +0200

    update

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -4,39 +4,29 @@ namespace TwentyToo\AutoTag\Setup;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\InstallDataInterface;
-use Psr\Log\LoggerInterface;
 
 class InstallData implements InstallDataInterface
 {
     /**
-     * @var LoggerInterface
-     */
-    private $logger;
-
-    /**
-     * InstallData constructor.
-     * @param LoggerInterface $logger
-     */
-    public function __construct(LoggerInterface $logger)
-    {
-        $this->logger = $logger;
-    }
-
-    /**
-     * @inheritdoc
+     * Install Data
+     *
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
      */
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        try {
-            // Your installation logic here
-            $this->logger->info('TwentyToo_AutoTag module installed successfully.');
-            
-            // Example of an error to log
-            throw new \Exception('An error occurred during module installation.');
-            
-        } catch (\Exception $e) {
-            // Log the error
-            $this->logger->error('Error during module installation: ' . $e->getMessage());
-        }
+        $setup->startSetup();
+
+        // Insert data into the twentytoo_tags table
+        $data = [
+            'order_id' => 123, // Replace with the actual order ID
+            'english_tags' => 'English Tags Value', // Replace with the actual English tags value
+            'arabic_tags' => 'Arabic Tags Value', // Replace with the actual Arabic tags value
+        ];
+
+        $setup->getConnection()->insert($setup->getTable('twentytoo_tags'), $data);
+
+        $setup->endSetup();
     }
 }
