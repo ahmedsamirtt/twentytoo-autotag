26c858cf6e7386fdac20bccaef1230638732afdf
test <test@9750-5007-6974>
2024-04-29 17:18:48 +0300

    update

--- Controller/Adminhtml/Twentytoo/Index.php
+++ Controller/Adminhtml/Twentytoo/Index.php
@@ -4,20 +4,24 @@ namespace TwentyToo\AutoTag\Controller\Adminhtml\Twentytoo;
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
+use Psr\Log\LoggerInterface;
 
 class Index extends Action
 {
     protected $resultPageFactory;
     protected $formKey;
+    protected $logger;
 
     public function __construct(
         Context $context,
         PageFactory $resultPageFactory,
-        \Magento\Framework\Data\Form\FormKey $formKey
+        \Magento\Framework\Data\Form\FormKey $formKey,
+        LoggerInterface $logger
     ) {
         parent::__construct($context);
         $this->resultPageFactory = $resultPageFactory;
         $this->formKey = $formKey;
+        $this->logger = $logger;
     }
 
     public function execute()
@@ -25,10 +29,14 @@ class Index extends Action
         $resultPage = $this->resultPageFactory->create();
         $resultPage->getConfig()->getTitle()->prepend(__('Welcome to TwentyToo'));
 
+        // Log form key generation
+        $formKey = $this->formKey->getFormKey();
+        $this->logger->info('Form key generated: ' . $formKey);
+
         // Add form key to the result page
         $resultPage->getLayout()->getBlock('twentytoo_welcome')
-            ->setData('form_key', $this->formKey->getFormKey());
-
+            ->setData('form_key', $formKey);
+        
         return $resultPage;
     }
 }
