b514966f66535d0a75e7de8e1203e57c8b567210
test <test@9750-5007-6974>
2024-04-29 16:51:22 +0300

    update

--- Controller/Adminhtml/Twentytoo/Index.php
+++ Controller/Adminhtml/Twentytoo/Index.php
@@ -8,19 +8,26 @@ use Magento\Framework\View\Result\PageFactory;
 class Index extends Action
 {
     protected $resultPageFactory;
+    protected $formKey;
 
     public function __construct(
         Context $context,
-        PageFactory $resultPageFactory
+        PageFactory $resultPageFactory,
+        \Magento\Framework\Data\Form\FormKey $formKey
     ) {
         parent::__construct($context);
         $this->resultPageFactory = $resultPageFactory;
+        $this->formKey = $formKey;
     }
 
     public function execute()
     {
         $resultPage = $this->resultPageFactory->create();
         $resultPage->getConfig()->getTitle()->prepend(__('Welcome to TwentyToo'));
+
+        // Add form key to the result page
+        $resultPage->getLayout()->getBlock('twentytoo_welcome')
+            ->setData('form_key', $this->formKey->getFormKey());
 
         return $resultPage;
     }
