7945cd88364b2ca6132bb887f3f6d491aee85c21
test <test@9750-5007-6974>
2024-04-29 10:29:55 +0300

    update

---
+++ AdminButton/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,13 @@
+<?php
+namespace Twentytoo\AdminButton\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+
+class Index extends Action
+{
+    public function execute()
+    {
+        echo "Hello Twentytoo";
+        exit;
+    }
+}
