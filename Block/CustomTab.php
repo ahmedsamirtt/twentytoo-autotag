3be69955aed582780d55401120706693e28056eb
test <test@9750-5007-6974>
2024-05-10 11:57:10 +0300

    update

--- Block/CustomTab.php
+++ Block/CustomTab.php
@@ -41,17 +41,20 @@ class CustomTab extends Template
         $results = $connection->fetchAll($select, $binds);
         $englishTags = json_decode($results[0]['english_tags'], true);
         $arabicTags = json_decode($results[0]['arabic_tags'], true);
-        $allTags = [
-            'english_tags' => $englishTags,
-            'arabic_tags' => $arabicTags
-        ];
 
-        // Get product instance
-        $product = $this->productFactory->create()->load($productId);
-        
-        // Log the results array
-        $this->logger->info('Results array:', $allTags);
+        // Determine the current store's locale code
+        $locale = $this->_storeManager->getStore()->getLocaleCode();
+
+        // Check if the locale indicates Arabic
+        $isArabic = (strpos($locale, 'ar_') === 0);
+
+        // Determine which set of tags to use based on the detected language
+        $selectedTags = $isArabic ? $arabicTags : $englishTags;
+
+        // Log the selected tags and product ID
+        $this->logger->info('Selected tags:', $selectedTags);
         $this->logger->info('Product ID:', ['product_id' => $productId]);
-        return $allTags;
+
+        return $selectedTags;
     }
 }
