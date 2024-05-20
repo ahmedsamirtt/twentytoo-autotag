2451ebfa3c392fde2702b1c43fb13721c0090fe8
test <test@9750-5007-6974>
2024-05-20 21:34:04 +0300

    update

--- Block/CustomTab.php
+++ Block/CustomTab.php
@@ -28,33 +28,40 @@ class CustomTab extends Template
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $resource = $objectManager->get(\Magento\Framework\App\ResourceConnection::class);
         $connection = $resource->getConnection();
-
+    
         // Get current product ID
         $productId = $this->getRequest()->getParam('id'); // Assuming you're getting product ID from request parameter
-
+    
         $select = $connection->select()->from($this->_tableName)
             ->where('order_id = :order_id');
-
-        // $staticOrderId = 'dress2';
+    
         $binds = [':order_id' => $productId];
-
+    
         $results = $connection->fetchAll($select, $binds);
+    
+        // Check if the query returned any results
+        if (empty($results)) {
+            $this->logger->warning('No tags found for product ID:', ['product_id' => $productId]);
+            return []; // Or return some default value or handle as necessary
+        }
+    
         $englishTags = json_decode($results[0]['english_tags'], true);
         $arabicTags = json_decode($results[0]['arabic_tags'], true);
-
+    
         // Determine the current store's locale code
         $locale = $this->_storeManager->getStore()->getLocaleCode();
-
+    
         // Check if the locale indicates Arabic
         $isArabic = (strpos($locale, 'ar_') === 0);
-
+    
         // Determine which set of tags to use based on the detected language
         $selectedTags = $isArabic ? $arabicTags : $englishTags;
-
+    
         // Log the selected tags and product ID
         $this->logger->info('Selected tags:', $selectedTags);
         $this->logger->info('Product ID:', ['product_id' => $productId]);
-
+    
         return $selectedTags;
     }
+    
 }
